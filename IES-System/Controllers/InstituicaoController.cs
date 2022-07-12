d0cffdef6142058c18ec4e14477a1887c4153925
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-07-11 22:40:20 -0300

    Implementation of the action that receives the model to insert the Create view

--- IES-System/Controllers/InstituicaoController.cs
+++ IES-System/Controllers/InstituicaoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using IES_System.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,11 +29,19 @@ namespace IES_System.Controllers
             },
         };
 
-        //GET: Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }        
 
         public IActionResult Index()
         {
